fd0af489b1fcefb9fe44a2d8593ea3aea443457d
Willy Villalobos <wvillalobos@cenat.ac.cr>
2017-04-25 23:22:58 -0600

    correcciones al collaterals

--- Experimento2/Collaterals.v
+++ Experimento2/Collaterals.v
@@ -49,30 +49,30 @@ endmodule
 
 //Multiplicador ejercicio 2 y 3
 
-module MULTIPLICADOR3 # (parameter SIZE=16)
-(
-	input wire [SIZE-2:2] Ci,
-	input wire [SIZE-1:0] iA,
-	input wire [SIZE-1:0] iB,
-	output reg Co,
-	output reg [SIZE:0] Res
-);
-	assign Ci[0] = 1'b0;
-	assign Co = Ci[SIZE];
-	
-	genvar i;
-	generate
-		for (i=1; i<SIZE-1, i=i+1)
-		begin: mult_cell
-			assign Res[0] = iA[0]&iB[0];
-			assign {Ci[i+1],Res[i]} = iA[i]&iB[0] + iA[i-1]&iB[1] + Ci[i];
-			assign {Ci[SIZE],Res[SIZE-1]} = iA[SIZE-1]&iB[1] + Ci[SIZE-1];
-		end
-	endgenerate
-	
-	
-	
-endmodule
+//module MULTIPLICADOR3 # (parameter SIZE=16)
+//(
+//	input wire [SIZE-2:2] Ci,
+//	input wire [SIZE-1:0] iA,
+//	input wire [SIZE-1:0] iB,
+//	output reg Co,
+//	output reg [SIZE:0] Res
+//);
+//	assign Ci[0] = 1'b0;
+//	assign Co = Ci[SIZE];
+//	
+//	genvar i;
+//	generate
+//		for (i=1; i<SIZE-1, i=i+1)
+//		begin: mult_cell
+//			assign Res[0] = iA[0]&iB[0];
+//			assign {Ci[i+1],Res[i]} = iA[i]&iB[0] + iA[i-1]&iB[1] + Ci[i];
+//			assign {Ci[SIZE],Res[SIZE-1]} = iA[SIZE-1]&iB[1] + Ci[SIZE-1];
+//		end
+//	endgenerate
+//	
+//	
+//	
+//endmodule
 
 //----------------------------------------------------------------------
 
